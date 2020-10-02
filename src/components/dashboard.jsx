8f67e7c688975cb7a6729763db1efb9108ab237a
CH M Anees Hussain Toor <aneeshussain334@gmail.com>
2020-10-03 00:44:03 +0500

    Refractor Nav Items

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button, Modal } from "react-bootstrap";
 import db from "../firebase";
+import Image from "../favicon.ico";
 
 let dbData = [];
 
@@ -58,7 +59,7 @@ class Dashboard extends Component {
 
   handleClose = () => this.setState({ show: false });
 
-  handleDelete = ({id}) => {
+  handleDelete = ({ id }) => {
     db.ref(`/quotes/${id}`).remove();
     dbData = [];
     this.getQuotes();
@@ -68,35 +69,30 @@ class Dashboard extends Component {
     return (
       <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">
+          <a class="navbar-brand" href="#">
+            <img
+              src={Image}
+              width="30"
+              height="30"
+              class="d-inline-block align-top"
+              alt=""
+            />
             Quotes Manager
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Dashboard <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#" onClick={this.handleShow}>
-                  Add a Quote
-                </a>
-              </li>
-            </ul>
-          </div>
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">
+                    Dashboard <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#" onClick={this.handleShow}>
+                    Add a Quote
+                  </a>
+                </li>
+              </ul>
         </nav>
+
         <div style={{ padding: "2%" }}>
           <Modal show={this.state.show} onHide={this.handleClose}>
             <Modal.Header closeButton>
